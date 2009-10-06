26f12e04a9911da97832b1914e3778991d6f0262
Ben Hall <Ben@Benhall.me.uk>
2009-10-06 17:07:51 +0100

    First prototype of MeerIIS

---
+++ meeriis.tests/CreateWebsiteTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Xunit;
+
+namespace Meeriis.tests
+{
+    public class CreateWebsiteTests
+    {
+        string test_site;
+
+        public CreateWebsiteTests()
+        {
+            string codeBase = @"C:\sourcecode\meeriis\meeriis.tests";
+            test_site = Path.Combine(codeBase, "test_site");
+        }
+
+        [Fact]
+        public void Can_create_iis_website_given_name()
+        {
+            Website website = new Website("localhost");
+            int websiteId = website.Create("test_site", test_site, 8888);
+            Assert.True(websiteId > 0);
+
+            string html = GetSite("http://localhost:8888/");
+
+            Assert.Contains("Hello World", html);
+        }
+
+        [Fact]
+        public void Can_get_if_website_exists()
+        {
+            Website website = new Website("localhost");
+            bool exist = website.Exist("test_site");
+            Assert.False(exist);
+        }
+
+        private string GetSite(string uri)
+        {
+            var request = HttpWebRequest.Create(new Uri(uri));
+            var response = request.GetResponse() as HttpWebResponse;
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            return reader.ReadToEnd();
+        }
+    }
+}
---
+++ meeriis/Website.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace Meeriis
+{
+    public class Website
+    {
+        private string Server;
+
+        public Website(string server)
+        {
+            Server = server;
+        }
+
+        public int Create(string name, string homeDirectory)
+        {
+            return Create(name, homeDirectory, 80);
+        }
+
+        public int Create(string name, string homeDirectory, int port)
+        {
+            DirectoryEntry w3svc = new DirectoryEntry(string.Format("IIS://{0}/w3svc", Server));
+          
+            object[] newsite = new object[] { name, new object[] { string.Format(":{0}:", port) }, homeDirectory };
+            int websiteId = (int)w3svc.Invoke("CreateNewSite", newsite);
+
+            DirectoryEntry website = GetWebsite(Server, websiteId);
+            SetSecurity(website);
+            SetASPnetVersion(website);
+            StartWebsite(website);
+
+            return websiteId;
+        }
+
+        private void SetSecurity(DirectoryEntry website)
+        {
+            website.Properties["AppPoolId"][0] = "DefaultAppPool";
+            website.Properties["AccessFlags"][0] = 512;
+            website.Properties["AccessRead"][0] = true;
+
+            //// set Application Name (friendly name)
+            //// doesn't seem to work...
+            //w3svc.Properties["AppFriendlyName"][0] = siteDescription;
+
+            website.Properties["AuthFlags"][0] = 5;
+            website.CommitChanges();
+        }
+
+        private DirectoryEntry GetWebsite(string server, int websiteId)
+        {
+            return new DirectoryEntry(string.Format("IIS://{0}/w3svc/{1}", server, websiteId));
+        }
+
+        private void SetASPnetVersion(DirectoryEntry website)
+        {
+            // upgrade Scriptmap to 2.0.50272 (ASP.NET version)
+            for (int prop = 0; prop < website.Properties["ScriptMaps"].Count; prop++)
+            {
+                website.Properties["ScriptMaps"][prop] = website.Properties["ScriptMaps"][prop].ToString().Replace("v1.1.4322", "v2.0.50727");
+            }
+
+            website.CommitChanges();
+        }
+
+        private void StartWebsite(DirectoryEntry website)
+        {
+            website.Invoke("Start", null);
+        }
+
+        public bool Exist(string websiteName)
+        {
+            bool result = false;
+
+            DirectoryEntry w3svc = new DirectoryEntry(string.Format("IIS://{0}/w3svc", Server));
+
+            foreach (DirectoryEntry site in w3svc.Children)
+            {
+                if (site.Properties["ServerComment"] != null)
+                {
+                    if (site.Properties["ServerComment"].Value != null)
+                    {
+                        if (string.Compare(site.Properties["ServerComment"].Value.ToString(), websiteName, false) == 0)
+                        {
+                            result = true;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
